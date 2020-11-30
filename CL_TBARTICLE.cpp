42f472747b91bab75031504fab06824c49528439
antoniorosemary <73826201+antoniorosemary@users.noreply.github.com>
2020-11-30 18:09:26 +0100

    article (comp)

--- CL_TBARTICLE.cpp
+++ CL_TBARTICLE.cpp
@@ -1,1 +1,46 @@
 #include "CL_TBARTICLE.h"
+
+
+namespace NS_Composants {
+
+	CL_TBARTICLE::CL_TBARTICLE() {
+		this->quantite_stock = 0;
+		this->Reference_article = "Defaut";
+
+	}
+	String^ CL_TBARTICLE::SELECT(void) {
+		return "SELECT * FROM dbo.Article";
+	}
+	String^ CL_TBARTICLE::INSERT(void) {
+		return "INSERT INTO dbo.Article" + "(quantite_stock, Reference_article) " + "VALUES('" + Convert::ToInt32(getquantite_stock()) + "', '" + getReference_article() + "'); SELECT @@IDENTITY;";
+	}
+	String^ CL_TBARTICLE::UPDATE(void) {
+		return "UPDATE dbo.Article set quantite_stock = ' " + Convert::ToInt32(getquantite_stock()) + "'" + ", Reference_article = '" + getReference_article()->ToString() + "'" + ", ID_article = '" + Convert::ToInt32(getID_article()) + "'";
+	}
+	String^ CL_TBARTICLE::DELETE(void) {
+		return "DELETE FROM dbo.Article WHERE ID_article =" + Convert::ToInt32(getID_article());
+	}
+
+
+	// GETTER
+	int CL_TBARTICLE::getID_article() {
+		return this->ID_article;
+	}
+	int CL_TBARTICLE::getquantite_stock() {
+		return this->quantite_stock;
+	}
+	String^ CL_TBARTICLE::getReference_article() {
+		return this->Reference_article;
+	}
+
+	// SETTER
+	void CL_TBARTICLE::setID_article(int ID_article) {
+		this->ID_article = ID_article;
+	}
+	void CL_TBARTICLE::setquantite_stock(int quantite_stock) {
+		this->quantite_stock = quantite_stock;
+	}
+	void CL_TBARTICLE::setReference_article(String^ Reference_article) {
+		this->Reference_article = Reference_article;
+	}
+}
--- CL_TBARTICLE.h
+++ CL_TBARTICLE.h
@@ -6,6 +6,25 @@ namespace NS_Composants {
 
 	ref class CL_TBARTICLE
 	{
+	private:
+		int quantite_stock;
+		int ID_article;
+		String^ Reference_article;
+	public:
+		CL_TBARTICLE();
+		String^ SELECT(void);
+		String^ INSERT(void);
+		String^ UPDATE(void);
+		String^ DELETE(void);
+
+		// GETTER
+		int getquantite_stock();
+		int getID_article();
+		String^ getReference_article();
+		// SETTER
+		void setquantite_stock(int);
+		void setID_article(int);
+		void setReference_article(String^);
 	};
 
 }
