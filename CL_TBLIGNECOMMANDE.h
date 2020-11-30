9b812d7551f0de6f204b84c2d08d2266b263774b
antoniorosemary <73826201+antoniorosemary@users.noreply.github.com>
2020-11-30 19:58:10 +0100

    commande ligne commande / debut

--- CL_TBLIGNECOMMANDE.h
+++ CL_TBLIGNECOMMANDE.h
@@ -9,3 +9,34 @@ namespace NS_Composants {
 	};
 
 }
+
+#pragma once
+
+namespace NS_Composants {
+
+	using namespace System;
+
+	ref class CL_TBARTICLE
+	{
+	private:
+		int quantite_stock;
+		int ID_article;
+		String^ Reference_article;
+	public:
+		CL_TBARTICLE();
+		String^ SELECT(void);
+		String^ INSERT(void);
+		String^ UPDATE(void);
+		String^ DELETE(void);
+
+		// GETTER
+		int getquantite_stock();
+		int getID_article();
+		String^ getReference_article();
+		// SETTER
+		void setquantite_stock(int);
+		void setID_article(int);
+		void setReference_article(String^);
+	};
+
+}
